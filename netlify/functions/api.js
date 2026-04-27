3f6900da5b4c04788f829a35a69e174fd9621380
Scot Salazar <scot.salazar@neoapps.com>
2026-04-27 15:09:38 +0800

    Fix PDF contract response encoding

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,7 +2,8 @@ const serverless = require('serverless-http');
 const app = require('../../server');
 
 const handler = serverless(app, {
-  basePath: '/.netlify/functions/api'
+  basePath: '/.netlify/functions/api',
+  binary: ['application/pdf', 'application/octet-stream']
 });
 
 exports.handler = async (event, context) => {
